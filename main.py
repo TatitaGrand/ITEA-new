bed3778755fa57f8195df55e24dd534a9fb04988
TatitaGrand <118078258+TatitaGrand@users.noreply.github.com>
2023-01-18 23:16:21 +0200

    Tanya Petrenko
    Homework 1

---
+++ main.py
@@ -0,0 +1,42 @@
+# Task 1: write code that printing your name into the console
+
+# Place your code here:
+
+print()
+print("Tatita")
+
+
+# Task 2:
+# Create a variable named data, and assign to it the value 255
+# Create a variable named payload, and assign to it the value "0x345654"
+# Create a variable named flag, and assign to it the value True
+# Create a variable named chs, and assign to it the value '!@$%#^&'
+
+# Place your code here:
+
+data = 255
+payload = "0x345654"
+flag = True
+chs = '!@$%#^&'
+
+print()
+print (data) 
+print (payload)
+print (flag)
+print (chs)
+
+
+# Task 3:
+# Print all variables in next format (help: use spaces):
+#       data value is    : 255
+#       payload value is : 0x345654
+#       flag value is    : True
+#       chs value is     : !@$%#^&
+
+# Place your code here:
+
+print()
+print("data value is    :"    , data) 
+print("payload value is :" , payload)
+print("flag value is    :"    , flag)
+print("chs value is     :"     , chs)
